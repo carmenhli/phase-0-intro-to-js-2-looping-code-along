d7f0345e5e9c6e0f2cf774d9b030a680bb49bbed
Carmen Li <cmnli95@gmail.com>
2023-10-09 16:23:29 -0400

    carmen pushing intro to js 2 loopin code along lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+// Assignment write cards //
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names){
+    const thankYouMessages = []
+    let i = 0; 
+    while (i < names.length) {
+        const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+        thankYouMessages.push(message)
+        i++;
+    }
+    return thankYouMessages;
+}
+
+const messages = writeCards(names)
+console.log(messages)
+
+// count down with console.log // 
+
+function countDown(number){
+    for (let i = number; i >=0; i--){
+        console.log(i)
+    }
+}
+countDown(5)
